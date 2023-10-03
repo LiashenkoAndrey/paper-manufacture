556a569cdb41304a7b1c7cf23744fbbfa26fe995
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-10-03 13:01:47 +0300

    edit

--- src/main/java/com/paper/config/MongoDbConfig.java
+++ src/main/java/com/paper/config/MongoDbConfig.java
@@ -1,7 +1,6 @@
 package com.paper.config;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.ConnectionString;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoClientFactoryBean;
@@ -11,7 +10,7 @@ public class MongoDbConfig {
 
     public @Bean MongoClientFactoryBean mongo() {
         MongoClientFactoryBean mongo = new MongoClientFactoryBean();
-        mongo.setPort(27017);
+        mongo.setConnectionString(new ConnectionString("mongodb+srv://makcimkakakakaka:komunist2023@cluster0.euyznd3.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp&authMechanism=SCRAM-SHA-1"));
         return mongo;
     }
 
